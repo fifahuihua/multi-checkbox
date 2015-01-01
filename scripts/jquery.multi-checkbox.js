aea3db1776053da01c3be569e3a193ca3b3d5de5
Antony ZHANG <huihua.zhang@dinglicom.com>
2015-01-01 23:02:14 +0800

    Add styles of multiple checkbox

--- scripts/jquery.multi-checkbox.js
+++ scripts/jquery.multi-checkbox.js
@@ -1,15 +1,6 @@
 (function ( $ ) {
  
-    $.fn.multicheckbox = function( options ) {
  
-        // This is the default options.
-        var settings = $.extend({
-            theme: "default-checkbox"
-        }, options );
- 
-        return this.addClass(settings.theme);
- 
-    };
- 
-}( jQuery ));
+//}( jQuery ));
+})
 
